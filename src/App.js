a1062409589fb3b02f2546a4addebc639da62475
hoang <giahoangth@gmail.com>
2019-12-20 16:23:37 +0700

    adding image assets and MenuComponent.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
-
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from "./components/MenuComponent";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">NoConfuse react</NavbarBrand>
           </div>
         </Navbar>
+        <Menu/>
       </div>
     );
   }
