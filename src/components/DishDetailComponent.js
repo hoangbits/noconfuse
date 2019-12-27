4b1780ab30e446d13133bb97faa63c2f54a20203
hoang <giahoangth@gmail.com>
2019-12-27 12:18:58 +0700

    week2-exercise2-functional components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -2,55 +2,52 @@ import React, { Component } from 'react';
 import { Card, CardBody, CardImg, CardText, CardTitle } from 'reactstrap';
 import moment from 'moment';
 
-class DishDetail extends Component {
-  renderDish(dish) {
-    return (
-      <Card>
-        <CardImg width="100%" src={dish.image} alt={dish.name} />
-        <CardBody>
-          <CardTitle>{dish.name}</CardTitle>
-          <CardText>{dish.description}</CardText>
-        </CardBody>
-      </Card>
-    );
-  }
+function RenderDish({ dish }) {
+  return (
+    <Card>
+      <CardImg width="100%" src={dish.image} alt={dish.name} />
+      <CardBody>
+        <CardTitle>{dish.name}</CardTitle>
+        <CardText>{dish.description}</CardText>
+      </CardBody>
+    </Card>
+  );
+}
 
-  renderComment(comments) {
-    const listComments = comments.map(comment => {
-      if (comment && comment.comment && comment.author && comment.date) {
-        return (
-          <div key={comment.id}>
-            <li>{comment.comment}</li>
-            <li>
-              -- {comment.author}, {moment(comment.date).format('MMM DD, YYYY')}
-            </li>
-          </div>
-        );
-      } else {
-        return <div />;
-      }
-    });
-    return <ul className="list-unstyled">{listComments}</ul>;
-  }
-
-  render() {
-    if (this.props.dish != null) {
+function RenderComment({ comments }) {
+  const listComments = comments.map(comment => {
+    if (comment && comment.comment && comment.author && comment.date) {
       return (
-        <div className="container">
-          <div className="row">
-            <div className="col-12 col-md-5 m-1">
-              {this.renderDish(this.props.dish)}
-            </div>
-            <div className="col-12 col-md-5 m-1">
-              {this.renderComment(this.props.dish.comments)}
-            </div>
-          </div>
+        <div key={comment.id}>
+          <li>{comment.comment}</li>
+          <li>
+            -- {comment.author}, {moment(comment.date).format('MMM DD, YYYY')}
+          </li>
         </div>
       );
     } else {
       return <div />;
     }
-  }
+  });
+  return <ul className="list-unstyled">{listComments}</ul>;
 }
+const DishDetail = props => {
+  if (props.dish != null) {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            <RenderDish dish={props.dish} />
+          </div>
+          <div className="col-12 col-md-5 m-1">
+            <RenderComment comments={props.dish.comments} />
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return <div />;
+  }
+};
 
 export default DishDetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
-import MenuItem from './MenuItem';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-class Menu extends Component {
-  render() {
-    const menu = this.props.dishes.map(dish => {
-      return (
-        <MenuItem key={dish.id} dish={dish} onClick={this.props.onClick} />
-      );
-    });
+function RenderMenuItem({ dish, onClick }) {
+  const memoizedOnClick = React.useCallback(() => {
+    onClick(dish.id);
+  }, []);
 
+  return (
+    <Card onClick={memoizedOnClick}>
+      <CardImg width="100%" src={dish.image} alt={dish.name} />
+      <CardImgOverlay>
+        <CardTitle>{dish.name}</CardTitle>
+      </CardImgOverlay>
+    </Card>
+  );
+}
+
+function Menu(props) {
+  const menu = props.dishes.map(dish => {
     return (
-      <div className="container">
-        <div className="row">{menu}</div>
+      <div className="col-12 col-md-5 m-1" key={dish.id}>
+        <RenderMenuItem dish={dish} onClick={props.onClick} />
       </div>
     );
-  }
+  });
+  return (
+    <div className="container">
+      <div className="row">{menu}</div>
+    </div>
+  );
 }
 
 export default Menu;
