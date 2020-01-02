ead7da686ed7a2ace1f1f75133d7710b5fc90b19
hoang <giahoangth@gmail.com>
2020-01-02 09:33:22 +0700

    week4-1.redux-combining reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,18 @@
-import { createStore } from "redux";
-import { Reducer, initialState } from "./reducer";
+import { combineReducers, createStore } from "redux";
+import {Dishes} from "./dishes";
+import {Comments} from "./comments";
+import {Promotions} from "./promotions";
+import {Leaders} from "./leaders";
 
 export const ConfigureStore = () => {
   const store = createStore(
-    Reducer, // reducer
-    initialState // out initialState
-    ,
-     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    combineReducers({
+      dishes: Dishes,
+      comments: Comments,
+      promotions: Promotions,
+      leaders: Leaders
+    }), // reducer
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
   return store;
 };
